419922576c35b76788a8e4380f67431152cc832f
Jens Balvig <jens@cookpad.com>
2012-11-07 16:15:20 +0900

    fixed factory_girl/active_admin problems

--- lib/utensils/factory_girl.rb
+++ lib/utensils/factory_girl.rb
@@ -1,1 +1,3 @@
-include FactoryGirl::Syntax::Methods
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+end
--- lib/utensils/version.rb
+++ lib/utensils/version.rb
@@ -1,3 +1,3 @@
 module Utensils
-  VERSION = "0.0.7"
+  VERSION = "0.0.8"
 end
