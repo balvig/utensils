6b017673c7e8f3aebbf712e8f8b0cdda59076943
Jens Balvig <jens@balvig.com>
2013-07-07 21:34:42 +0900

    support for Rails4

--- lib/utensils/capybara_extensions.rb
+++ lib/utensils/capybara_extensions.rb
@@ -1,4 +1,5 @@
+identifier = defined?(ActionView::RecordIdentifier) ? ActionView::RecordIdentifier : ActionController::RecordIdentifier
 Capybara.add_selector :record do
-  xpath { |record| XPath.css("#" + ActionController::RecordIdentifier.dom_id(record)) }
+  xpath { |record| XPath.css("#" + identifier.dom_id(record)) }
   match { |record| record.is_a?(ActiveRecord::Base) }
 end
