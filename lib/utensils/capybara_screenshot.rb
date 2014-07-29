edd71a7bd9d8ce7615b5ecba2c45bfdf07aa2730
Jens Balvig <jens@balvig.com>
2014-07-29 16:13:52 +0200

    update config for capybara screenshot

--- lib/utensils/capybara_screenshot.rb
+++ lib/utensils/capybara_screenshot.rb
@@ -1,4 +1,4 @@
 require 'capybara-screenshot/rspec'
 Capybara.save_and_open_page_path = 'public/tmp'
-Capybara::Screenshot.append_screenshot_path = false
+Capybara::Screenshot::RSpec.add_link_to_screenshot_for_failed_examples = false
 Capybara::Screenshot.append_timestamp = false
