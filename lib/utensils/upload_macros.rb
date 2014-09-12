517d6d1d9705710638a17934240b079d70e66860
Jens Balvig <jens@balvig.com>
2014-09-12 15:33:20 +0200

    adds support for fixture file uploads

--- lib/utensils/upload_macros.rb
+++ lib/utensils/upload_macros.rb
@@ -1,11 +1,17 @@
 module Utensils
   module UploadMacros
+    include ActionDispatch::TestProcess
+
+    def fixture_file_upload(filename)
+      super fixture_file_path(filename)
+    end
+
     def fixture_file(filename)
-      File.new(fixture_file_path(filename))
+      File.new fixture_file_path(filename)
     end
 
     def fixture_file_path(filename)
-      Rails.root.join('spec','fixtures',filename)
+      Rails.root.join('spec', 'fixtures', filename)
     end
   end
 end
