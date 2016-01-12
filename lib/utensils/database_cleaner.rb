620afa372c40a8ec53a76b0fd173ade234744b45
kinopyo <fantasyday@gmail.com>
2016-01-12 16:00:21 +0900

    Set mongoid strategy explicitly

--- lib/utensils/database_cleaner.rb
+++ lib/utensils/database_cleaner.rb
@@ -1,6 +1,14 @@
 RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner[:mongoid].strategy = :truncation
+    DatabaseCleaner[:active_record].strategy = :transaction
+
+    DatabaseCleaner.clean_with :truncation
+  end
+
   config.around(:each) do |example|
-    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
+    DatabaseCleaner[:active_record].strategy = example.metadata[:js] ? :truncation : :transaction
+
     DatabaseCleaner.cleaning do
       example.run
     end
