3530d4b038d807e454637a2bea9af8ae705ee921
kinopyo <fantasyday@gmail.com>
2016-01-13 11:31:23 +0900

    Simplify the config

--- lib/utensils/database_cleaner.rb
+++ lib/utensils/database_cleaner.rb
@@ -1,10 +1,4 @@
 RSpec.configure do |config|
-  config.before(:suite) do
-    DatabaseCleaner[:active_record].strategy = :transaction
-
-    DatabaseCleaner.clean_with :truncation
-  end
-
   config.around(:each) do |example|
     DatabaseCleaner[:active_record].strategy = example.metadata[:js] ? :truncation : :transaction
 
