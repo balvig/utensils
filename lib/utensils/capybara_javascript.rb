f0d8889f30d2e331d514aef0f8abdba98471176a
Leonard Chin <l.g.chin@gmail.com>
2016-07-27 11:22:36 +0900

    Add alternate poltergeist_debug driver
    Running rspec with the `POLTERGEIST_DEBUG` envvar set will enable
    Poltergeist's `inspector` option for remote debugging.
    
    Inserting `page.driver.debug` in examples will launch a browser to allow
    debugging with the Webkit Inspector
    
    Reference: http://jonathanleighton.com/articles/2012/poltergeist-0-6-0/

--- lib/utensils/capybara_javascript.rb
+++ lib/utensils/capybara_javascript.rb
@@ -1,2 +1,10 @@
 require 'capybara/poltergeist'
-Capybara.javascript_driver = :poltergeist
+Capybara.register_driver :poltergeist_debug do |app|
+  Capybara::Poltergeist::Driver.new(app, inspector: true)
+end
+
+Capybara.javascript_driver = if ENV["POLTERGEIST_DEBUG"]
+                               :poltergeist_debug
+                             else
+                               :poltergeist
+                             end
