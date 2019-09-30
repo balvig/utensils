361ec65848f2f7ac9979835db0f9beeae19440b2
Prem Sichanugrist <s@sikac.hu>
2019-09-30 22:53:33 +0900

    Remove capybara-screenshot utensils (#13)

--- lib/utensils/capybara_screenshot.rb
+++
@@ -1,4 +0,0 @@
-require 'capybara-screenshot/rspec'
-Capybara.save_path = 'public/tmp'
-Capybara::Screenshot::RSpec.add_link_to_screenshot_for_failed_examples = false
-Capybara::Screenshot.append_timestamp = false
