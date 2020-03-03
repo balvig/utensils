5d6f44d36427400b5f880513728989df7dc139ff
Christian Bruckmayer <christian-bruckmayer@cookpad.com>
2020-03-03 08:40:39 +0100

    Update lazy loaded image matcher to match data-src (#15)

--- lib/utensils/custom_matchers.rb
+++ lib/utensils/custom_matchers.rb
@@ -94,7 +94,7 @@ module Utensils
         end
 
         def find_lazy_loaded_image
-          @page.has_css?("img[data-original$='#{@image}']")
+          @page.has_css?("img[data-src$='#{@image}']")
         end
 
         def find_dragonfly_image
